01a1fc49c8b759128e9dbd06b952cd08224ccab0
aymaaar <a24aymramagu@inspedralbes.cat>
2025-12-12 09:04:26 +0100

    frontend electron

---
+++ vuetify-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <VoiceCommander />
+      
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  // Importamos el componente. 
+  // Asegúrate de que el archivo VoiceCommander.vue existe en src/components/
+  import VoiceCommander from './components/VoiceCommander.vue'
+</script>
